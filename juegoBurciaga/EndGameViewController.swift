664e15cedb4ea398e1bb0dd28c99f9185a32e6ee
Gael <ack3stx10@gmail.com>
2025-03-21 09:45:08 -0600

    agregando datos de la partida ganada

--- juegoBurciaga/EndGameViewController.swift
+++ juegoBurciaga/EndGameViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class EndGameViewController: UIViewController {
 
+    
+    var progreso = Datos.sharedDatos()
+
+    @IBOutlet weak var lblPremio: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        lblPremio.text = "$ " + String( progreso.valorGanado)
 
         // Do any additional setup after loading the view.
     }
